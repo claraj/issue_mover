3f6843381ab592139d609e8e202eb95f96fccaa3
Clara <10088152+claraj@users.noreply.github.com>
2021-01-29 08:27:37 -0600

    Clarify what to enter for  repo name

--- issue_mover.py
+++ issue_mover.py
@@ -17,7 +17,7 @@ username = input('enter your username: ') # or put your GitHub username here
 password = getpass.getpass(prompt='enter password: ')  # window users, password.getpass doesn't work for you. You'll need to replace with password= 'your password'
 
 
-base_repo_name = input('enter the repo name e.g. book_wishlist ') 
+base_repo_name = input('enter the repo name e.g. readinglist for a repo located at https://github.com/claraj/readinglist/') 
 
 source_url = 'https://api.github.com/repos/claraj/{}/issues'.format(base_repo_name)
 
