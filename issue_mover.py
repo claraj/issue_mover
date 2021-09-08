631865323ee5a877ee1368b2c36585010efb8428
Clara <10088152+claraj@users.noreply.github.com>
2021-09-08 16:52:17 -0500

    Update issue_mover.py

--- issue_mover.py
+++ issue_mover.py
@@ -7,38 +7,36 @@ import sys
 BOLD = '\033[32;1m'  # Bold green text
 END = '\033[0m'  # Reset to default color
 
-def create_label(label):
-    l = Label.Label()
-    l.edit(label['name'], label['color'])
-    return l
-
 
 username = input('enter your username: ') # or put your GitHub username here
-password = getpass.getpass(prompt='enter password: ')  # window users, password.getpass doesn't work for you. You'll need to replace with password= 'your password'
+
+# Window users, password.getpass doesn't work for you. It may work in git bash? You'll need to replace with password = 'your password'
+password = getpass.getpass(prompt='enter password: ')  
 
 
-base_repo_name = input('enter the repo name e.g. readinglist for a repo located at https://github.com/claraj/readinglist/') 
+base_repo_name = input('Enter the original repo name e.g. "readinglist" for a repo located at https://github.com/claraj/readinglist/\n') 
 
-source_url = 'https://api.github.com/repos/claraj/{}/issues'.format(base_repo_name)
+source_url = f'https://api.github.com/repos/claraj/{base_repo_name}/issues'
 
+# Authenticate to GitHub with your credentials
 github = Github(username, password)
 
 base_repo = github.get_repo('claraj/' + base_repo_name)
 base_issues = base_repo.get_issues()
 
-
 your_repo = github.get_repo(username + "/" + base_repo_name)
+
+
+print('Creating labels in your repository')
 
 labels = base_repo.get_labels()
 
 try:
-
-    print('Creating labels in your repository')
     for label in labels:
         try:
             your_repo.create_label(label.name, label.color)
         except GithubException:
-            print('Error creating label %s. Does it already exist?' % label.name)
+            print(f'Error creating label {label.name}. Does it already exist? Continuing.')
 
 
 except BadCredentialsException:
@@ -47,7 +45,7 @@ except BadCredentialsException:
 
 
 print('Creating issues in your repository')
-
+    
 for issue in base_issues:
     try:
         your_repo.create_issue(issue.title, body=issue.body, labels=issue.labels)
